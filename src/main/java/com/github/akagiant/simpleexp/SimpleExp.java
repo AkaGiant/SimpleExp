8608f69b9e94a9cde03e9f18f33e6b7ceb6ce412
AkaGiant <akagiant01@gmail.com>
2023-05-08 13:54:12 +0100

    Initial Commit

--- src/main/java/com/github/akagiant/simpleexp/SimpleExp.java
+++ src/main/java/com/github/akagiant/simpleexp/SimpleExp.java
@@ -25,18 +25,31 @@
 
 package com.github.akagiant.simpleexp;
 
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPIBukkitConfig;
+import dev.jorel.commandapi.CommandAPIConfig;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SimpleExp extends JavaPlugin {
 
+    private static Plugin plugin;
+
+    @Override
+    public void onLoad() {
+        CommandAPI.onLoad(new CommandAPIBukkitConfig(this).silentLogs(true));
+    }
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        plugin = this;
+        CommandAPI.onEnable();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        CommandAPI.onDisable();
     }
 }
